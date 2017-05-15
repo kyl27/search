d2da8547b08099f29acd7634ae25c9de7eeb5afe
Kevin Li <kyl27@cornell.edu>
2017-05-14 23:09:36 -0400

    Load directories by command

--- main.cpp
+++ main.cpp
@@ -15,12 +15,15 @@ using namespace boost::filesystem;
 
 struct Node {
     map<char, Node*> children;
-    map<int, set<int> > occurrences;
+    map<long, set<long> > occurrences;
     bool terminal;
 };
 
-map<int, string> files;
+long tokens, newTokens;
+map<long, string> files;
 Node *root;
+
+string helpStr = "Commands: load <directory> | query <string> | help | quit";
 
 
 bool isTokenDelimiter(char c) {
@@ -35,18 +38,21 @@ bool isWordDelimiter(char c) {
     return isTokenDelimiter(c) && isCamlDelimiter(c) && (c < '-' || c > ':') && c != '_';
 }
 
-void insertToken(Node *n, const string &token, int fileIndex, int line) {
+void insertToken(Node *n, const string &token, long fileIndex, long line) {
     for (size_t i = 0; i < token.length(); ++i) {
-        if (n->children.find(token[i]) == n->children.end()) {
-            n->children.emplace(token[i], new Node());
+        if (n->children.count(token[i]) == 0) {
+            n->children[token[i]] = new Node();
         }
         n = n->children[token[i]];
     }
-    n->occurrences[fileIndex].insert(line);
+    if (n->occurrences[fileIndex].insert(line).second) {
+        ++tokens;
+    }
+    ++newTokens;
     n->terminal = true;
 }
 
-void insertWord(const string &word, int fileIndex, int line) {
+void insertWord(const string &word, long fileIndex, long line) {
     for (size_t i = 0; i < word.length(); ++i) {
         if (isTokenDelimiter(word[i])) {
             if (isCamlDelimiter(word[i])) {
@@ -59,26 +65,32 @@ void insertWord(const string &word, int fileIndex, int line) {
     insertToken(root, word, fileIndex, line);
 }
 
+void printResults(Node *n) {
+    for (auto it : n->occurrences) {
+        string &filepath = files[it.first];
+        std::ifstream file (filepath, ios::in);
+        if (file.is_open()) {
+            long lineNo = 1;
+            string line;
+            for (auto it_line : it.second) {
+                while (lineNo != it_line) {
+                    file.ignore(numeric_limits<streamsize>::max(), '\n');
+                    ++lineNo;
+                }
+                getline(file, line);
+                cout << filepath << ':' << it_line << ':' << line << endl;
+                ++lineNo;
+            }
+            file.close();
+        } else {
+            cout << "Failed to open file: " << filepath << endl;
+        }
+    }
+}
+
 void printTerminals(Node *n, string &s) {
     if (n->terminal) {
-        for (auto it : n->occurrences) {
-            string &filepath = files[it.first];
-            std::ifstream file (filepath, ios::in);
-            if (file.is_open()) {
-                int lineNo = 0;
-                string line;
-                for (auto it_line : it.second) {
-                    while (lineNo != it_line) {
-                        ++lineNo;
-                        getline(file, line);
-                    }
-                    cout << filepath << ':' << it_line << ':' << line << endl;
-                }
-                file.close();
-            } else {
-                cout << "Failed to open file: " << filepath << endl;
-            }
-        }
+        printResults(n);
     }
     for (auto it : n->children) {
         s += it.first;
@@ -89,8 +101,8 @@ void printTerminals(Node *n, string &s) {
 
 bool query(Node *n, string &query) {
     for (size_t i = 0; i < query.length(); ++i) {
-        if (n->children.find(query[i]) != n->children.end()) {
-            n = n->children.find(query[i])->second;
+        if (n->children.count(query[i])) {
+            n = n->children[query[i]];
         } else {
             cout << "No results for query: " << query << endl;
             return false;
@@ -101,22 +113,22 @@ bool query(Node *n, string &query) {
     return true;
 }
 
-int countTokens(Node *n) {
-    int count = n->terminal ? 1 : 0;
+long countTokens(Node *n) {
+    long count = n->terminal ? 1 : 0;
     for (auto it : n->children) {
         count += countTokens(it.second);
     }
     return count;
 }
 
-void indexFile(const string &filepath, int fileIndex) {
+void indexFile(const string &filepath, long fileIndex) {
     char c;
     string s;
-    int line = 1;
+    long line = 1;
     std::ifstream file (filepath, ios::in);
     if (file.is_open()) {
         files.emplace(fileIndex, filepath);
-        while (file.get(c) && c <= '~') {
+        while (file.get(c) && c > 0 && c < '~') {
             if (isWordDelimiter(c) && s.length()) {
                 insertWord(s, fileIndex, line);
                 s.clear();
@@ -133,24 +145,21 @@ void indexFile(const string &filepath, int fileIndex) {
     }
 }
 
-int iterateFiles(const string &path) {
-    int numFiles = 0;
-    cout << "Root path: " << path << endl;
+long iterateFiles(const string &path) {
+    long numFiles = 0;
+    cout << "Indexing root directory: " << path << endl;
     recursive_directory_iterator it(path), end;
     while (it != end)
     {
         if (it->path().stem().string().length() == 0 || is_symlink(*it)) {
             it.no_push();
-            ++it;
-            continue;
         } else if (is_directory(*it)) {
-            cout << "Processing directory: " << it->path().string() << endl;
-            ++it;
-            continue;
+            cout << "Indexing subdirectory: " << it->path().string() << endl;
+        } else {
+            indexFile(it->path().string(), numFiles);
+            ++numFiles;
         }
-        indexFile(it->path().string(), numFiles);
         ++it;
-        ++numFiles;
     }
     return numFiles;
 }
@@ -158,20 +167,31 @@ int iterateFiles(const string &path) {
 int main(int argc, char *argv[]) {
     root = new Node();
 
-    string s (argv[1]);
-    clock_t start = clock();
-    iterateFiles(s);
-    start = clock() - start;
+    string s;
 
-    cout << "Trie construction time (s) : "
-         << (double) start / CLOCKS_PER_SEC << endl;
+    while (true) {
+        cout << "Input command or \"help\":" << endl;
+        cin >> s;
 
-    cout << "Tokens added: " << countTokens(root) << endl;
+        if (s == "quit") {
+            break;
+        } else if (s == "help") {
+            cout << helpStr << endl;
+        } else if (s == "load" && cin >> s && is_directory(s)) {
+            newTokens = 0;
+            clock_t start = clock();
+            long numFiles = iterateFiles(s);
+            start = clock() - start;
 
-    cout << "Input query:" << endl;
-    while (cin >> s && s != "done") {
-        query(root, s);
-        cout << "Input query:" << endl;
+            cout << newTokens << " tokens added from "
+                 << numFiles << " files in "
+                 << (double) start / CLOCKS_PER_SEC << " seconds " << endl;
+            cout << "Total unique tokens: " << tokens << endl;
+        } else if (s == "query" && cin >> s) {
+            query(root, s);
+        } else {
+            cout << "Invalid command or directory does not exist." << endl;
+        }
     }
     return 0;
 }
